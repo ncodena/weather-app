7c5f1fd14845ec19c1070f5e87e89f217dde5b9d
Nuria <c.odena.nuria@gmail.com>
2020-05-09 14:28:32 +0200

    Get Weather API Call

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,5 +1,23 @@
 const key = 'qEM5zIGDv4POEZGASmbBpXVdvjDQBpbi';
 
+// Get weather information
+
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+
+    const data = await response.json();
+
+    return data[0];
+};
+
+getWeather('249758')
+
+// Get city information
 const getCity = async (city) => {
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
 
@@ -13,5 +31,9 @@ const getCity = async (city) => {
 };
 
 getCity('amsterdam')
-    .then(data => console.log(data))
+    .then(data => {
+        return getWeather(data.Key);
+    }).then(data => {
+        console.log(data)
+    })
     .catch(err => console.log(err))
