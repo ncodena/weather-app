6917f8467c714acee2ac8728744742ea9245f2f8
Nuria <c.odena.nuria@gmail.com>
2020-05-09 15:08:06 +0200

    updating ui

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,34 @@
 const cityForm = document.querySelector('form');
 
+const card = document.querySelector('.card');
+
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+
+    const cityDetails = data.cityDetails;
+
+    const weather = data.weather;
+
+    // Update details template
+
+    details.innerHTML = `
+
+        <h5 class="my-3">${cityDetails.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+            <div class="display-4 my-4">
+                <span>${weather.Temperature.Metric.Value}</span>
+                <span>&deg;C</span>
+            </div>
+            `;
+
+            // Removinf d-none class if present
+
+            if(card.classList.contains('d-none')){
+                card.classList.remove('d-none')
+            }
+};
+
 const updateCity = async (city) => {
 
     const cityDetails = await getCity(city)
@@ -7,8 +36,9 @@ const updateCity = async (city) => {
     const weather = await getWeather(cityDetails.Key);
 
     return {
-        cityDetails: cityDetails,
-        weather: weather
+        // Object shorthand notation => when we have a key and a property value that share the same name, we can delete one and it does the same thing
+        cityDetails,
+        weather
     };
 
 }
@@ -27,6 +57,6 @@ cityForm.addEventListener('submit', e => {
     // Update UI with new city
 
     updateCity(city)
-    .then(data => console.log(data))
+    .then(data => updateUI(data))
     .catch(err => console.log(err))
 })
