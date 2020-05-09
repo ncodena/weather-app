efc2361d06b56476f32ad25be030d3c1d3327984
Nuria <c.odena.nuria@gmail.com>
2020-05-09 14:15:47 +0200

    Get City API Call

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,1 +1,17 @@
 const key = 'qEM5zIGDv4POEZGASmbBpXVdvjDQBpbi';
+
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+
+    const data = await response.json();
+
+    return data[0]
+};
+
+getCity('amsterdam')
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
